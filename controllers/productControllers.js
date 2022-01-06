5e2277a7730b39e59d26188a7bd3920624d4ed9a
Vinit Raut <vinit.coder@gmail.com>
2022-01-07 01:30:26 +0530

    Add: Get all products and single product by ID API

---
+++ controllers/productControllers.js
@@ -0,0 +1,44 @@
+import { StatusCodes } from 'http-status-codes';
+import Product from '../models/productModel.js';
+
+/**
+ * @desc    Get all products
+ * @route   GET /api/v1/products
+ * @access  Public
+ */
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(StatusCodes.OK).json({
+      products,
+    });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: 'error',
+      error: error.message,
+    });
+  }
+};
+
+/**
+ * @desc    Get single product
+ * @route   GET /api/v1/products/:id
+ * @access  Public
+ */
+const getSingleProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    res.status(StatusCodes.OK).json({
+      product,
+    });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: 'error',
+      error: error.message,
+    });
+  }
+};
+
+export { getAllProducts, getSingleProduct };
---
+++ routes/productRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import {
+  getAllProducts,
+  getSingleProduct,
+} from '../controllers/productControllers.js';
+const router = express.Router();
+
+router.route('/').get(getAllProducts);
+router.route('/:id').get(getSingleProduct);
+
+export default router;
