60c639a5c8082889b5b8e4ef24260dcc10f5e5f8
Vinit Raut <vinit.coder@gmail.com>
2022-01-21 00:27:29 +0530

    Display orders on the profile page

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,21 +1,96 @@
+import moment from 'moment';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllOrders } from '../store/slices/orders';
+import { FaCheck, FaTimes } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 const Orders = () => {
   const dispatch = useDispatch();
   const { allOrders, loading } = useSelector((state) => state.orders);
+  const history = useNavigate();
 
   useEffect(() => {
     dispatch(fetchAllOrders());
   }, []);
 
   if (loading) return <p>Loading...</p>;
-  console.log(allOrders);
 
   return (
-    <div className="flex justify-center items-center w-full mt-24">
-      <h1>Orders</h1>
+    <div className="flex flex-col items-center w-full mt-24">
+      <h2 className="text-center text-2xl font-semibold text-gray-600 mb-4">
+        My Orders
+      </h2>
+
+      <table className="min-w-full border shadow-lg">
+        <thead className="bg-white border-b">
+          <tr>
+            <th
+              scope="col"
+              className="text-sm font-medium text-blue-600 px-6 py-4 text-left"
+            >
+              Order ID
+            </th>
+            <th
+              scope="col"
+              className="text-sm font-medium text-blue-600 px-6 py-4 text-left"
+            >
+              Date
+            </th>
+            <th
+              scope="col"
+              className="text-sm font-medium text-blue-600 px-6 py-4 text-left"
+            >
+              Total
+            </th>
+            <th
+              scope="col"
+              className="text-sm font-medium text-blue-600 px-6 py-4 text-left"
+            >
+              Paid
+            </th>
+            <th
+              scope="col"
+              className="text-sm font-medium text-blue-600 px-6 py-4 text-left"
+            >
+              Delivered
+            </th>
+            <th
+              scope="col"
+              className="text-sm font-medium text-blue-600 px-6 py-4 text-left"
+            ></th>
+          </tr>
+        </thead>
+        <tbody>
+          {allOrders?.map((order) => (
+            <tr key={order?._id} className="bg-gray-100 border-b">
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                {order?._id}
+              </td>
+              <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {moment(order?.paidAt).format(`DD-MM-YYYY, HH:mm`)}
+              </td>
+              <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {order?.amountPaid}
+              </td>
+              <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {order?.paidAt ? <FaCheck /> : <FaTimes />}
+              </td>
+              <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {order?.isDelivered ? <FaCheck /> : <FaTimes />}
+              </td>
+              <td className="flex text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                <button
+                  className="w-full bg-blue-500 text-indigo-100 py-2 px-4 rounded-md tracking-wide"
+                  onClick={() => history(`/orders/${order?._id}`)}
+                >
+                  Details
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
